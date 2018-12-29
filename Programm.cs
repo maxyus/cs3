e13ab0d5b1eb3b0f7d11542d94c486f3328a362f
maxyus <46244807+maxyus@users.noreply.github.com>
2018-12-29 17:33:53 +0300

    Add files via upload

---
+++ Programm.cs
@@ -0,0 +1,14 @@
+using System;
+using MyCollections;
+
+    public class Programm
+    {
+        public static void Main()
+        {
+            MyStack<int> stack = new MyStack<int>();
+            stack.Push(1);
+        stack.Push(1);
+        Console.WriteLine(stack.Length);
+        Console.ReadKey();
+        }
+    }
